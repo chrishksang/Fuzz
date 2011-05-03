0d0c9fc49661ef58280eb40954be293f5a9a5130
Chris How Kin Sang <chris.howkinsang@gmail.com>
2011-05-04 00:55:22 +0100

    Added sepia tone option to plugin.

--- js/jquery.fuzz.js
+++ js/jquery.fuzz.js
@@ -11,6 +11,7 @@
         // default settings
         var defaults = {
             color: false,
+            sepia: false,
             size: 100,
             opacity: 0.075
             },
@@ -33,32 +34,59 @@
                 r, g, b,
                 a = settings.opacity;
 
-                // prevent extremely large sizes
-                if (settings.size > 500) {
-                    settings.size = 500;
-                }
+            // prevent extremely large sizes
+            if (settings.size > 500) {
+                settings.size = 500;
+            }
 
-                canvasBg.width = settings.size;
-                canvasBg.height = settings.size;
+            canvasBg.width = settings.size;
+            canvasBg.height = settings.size;
 
-                for (x = 0; x < canvasBg.width; x++) {
+            for (x = 0; x < canvasBg.width; x++) {
 
-                    for (y = 0; y < canvasBg.width; y++) {
+                for (y = 0; y < canvasBg.width; y++) {
 
-                        r = Math.floor(Math.random() * 255);
-                        g = Math.floor(Math.random() * 255);
-                        b = Math.floor(Math.random() * 255);
-                        
-                        if (!settings.color) {
-                            bgCtx.fillStyle = 'rgba(' + r + ', ' + r + ', ' + r + ', ' + a + ')';
-                        } else {
-                            bgCtx.fillStyle = 'rgba(' + r + ', ' + g + ', ' + b + ', ' + a + ')';
-                        }
+                    r = Math.floor(Math.random() * 255);
+                    g = Math.floor(Math.random() * 255);
+                    b = Math.floor(Math.random() * 255);
 
-                        bgCtx.fillRect(x, y, 1, 1);
+                    if (!settings.color) {
+                        bgCtx.fillStyle = 'rgba(' + r + ', ' + r + ', ' + r + ', ' + a + ')';
+                    } else {
+                        bgCtx.fillStyle = 'rgba(' + r + ', ' + g + ', ' + b + ', ' + a + ')';
                     }
 
+                    bgCtx.fillRect(x, y, 1, 1);
                 }
+
+            }
+
+            // optionally add a sepia tone
+            if (settings.sepia) {
+
+                var imgData = bgCtx.getImageData(0, 0, canvasBg.width, canvasBg.height),
+				pxData = imgData.data, // put in an array to easily iterate over it
+				pxLength = pxData.length;
+
+                    for (var i = 0; i < pxLength; i+=4) {
+                        var r = pxData[i],
+                            g = pxData[i + 1],
+                            b = pxData[i + 2],
+                            sR = r * .393 + g * .769 + b * .189,
+                            sG = r * .349 + g * .686 + b * .168,
+                            sB = r * .272 + g * .534 + b * .131;
+
+                        pxData[i] = sR;
+                        pxData[i + 1] = sG;
+                        pxData[i + 2] = sB;
+
+                    }
+
+                imgData.data = pxData; // now we need to put the array back in the object
+
+                bgCtx.putImageData(imgData, 0, 0);
+
+            }
 
             // attach the canvas to each selected element's background image
             this.each(function(){
